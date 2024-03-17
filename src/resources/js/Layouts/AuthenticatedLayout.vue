caced8fe9a4af29ddce2d6b58608512b265cbb2b
thomazjb <thomaz.jb@hotmail.com>
2024-03-17 00:50:04 -0300

    improving background

--- src/resources/js/Layouts/AuthenticatedLayout.vue
+++ src/resources/js/Layouts/AuthenticatedLayout.vue
@@ -12,7 +12,7 @@ const showingNavigationDropdown = ref(false);
 
 <template>
     <div>
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 bg-cover bg-center" style="background-image: url('/desert.png')">
             <nav class="bg-white border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -138,7 +138,7 @@ const showingNavigationDropdown = ref(false);
 
             <!-- Page Heading -->
             <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
             </header>
--- src/resources/js/Pages/Welcome.vue
+++ src/resources/js/Pages/Welcome.vue
@@ -49,7 +49,7 @@ export default {
 <template>
 
     <Head title="Home" />
-    <div class="bg-gray-50 text-black/50">
+    <div class="bg-cover bg-center" style="background-image: url('/desert.png')">
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
