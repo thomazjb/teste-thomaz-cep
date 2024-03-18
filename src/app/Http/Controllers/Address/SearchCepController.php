41be708ec2772627da0b6fca80c93cac7afe9171
thomazjb <thomaz.jb@hotmail.com>
2024-03-18 16:43:05 -0300

    upgrading viacep

--- src/app/Http/Controllers/Address/SearchCepController.php
+++ src/app/Http/Controllers/Address/SearchCepController.php
@@ -11,28 +11,21 @@ class SearchCepController extends Controller
 {
     public function search(Request $request)
     {
-        // Validação básica do CEP
+
         $request->validate([
             'cep' => 'required|regex:/^\d{5}-\d{3}$/'
         ]);
 
         $cep = str_replace('-', '', $request->input('cep'));
-
-        // Cria uma instância do GuzzleHttp\Client
         $client = new Client();
-
-        // Faz a requisição para o serviço do ViaCEP
         $response = $client->get("https://viacep.com.br/ws/{$cep}/json/");
 
-        // Verifica se a requisição foi bem sucedida
         if ($response->getStatusCode() === 200) {
-            // Converte a resposta JSON para um array associativo
-            $data = json_decode($response->getBody(), true);
 
-            // Retorna os dados em formato JSON
+            $data = json_decode($response->getBody(), true);
             return Inertia::render('Welcome', ['data' => $data]);
         } else {
-            // Retorna uma resposta de erro em JSON
+
             return Inertia::render('Welcome', ['error' => 'Erro ao consultar o serviço ViaCEP']);
         }
     }
